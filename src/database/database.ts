cc8d1ece95b7dea860960a6a9475fa9dfbb3d538
Thays <thaysomine@gmail.com>
2022-07-14 12:04:07 -0300

    fix deploy

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,15 +3,6 @@ import pg from "pg";
 dotenv.config();
 
 const { Pool } = pg;
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL
-};
-
-if(process.env.MODE === "PROD") {
-  configDatabase.ssl = {
-    rejectUnauthorized: false
-  }
-}
-
-const connection = new Pool(configDatabase);
-export default connection;
+export const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
