82cdd480d791eceb609c8721d2ec47d89b171a05
Thays <thaysomine@gmail.com>
2022-07-15 03:10:18 -0300

    fix: deploy

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,6 +3,9 @@ import pg from "pg";
 dotenv.config();
 
 const { Pool } = pg;
-export const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const connection = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	...(process.env.PROD_MODE && {
+		ssl: { rejectUnauthorized: false },
+	}),
+})
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
 export default function errorHandler (error, req: Request, res: Response, next: NextFunction) {
-    console.log(error);
-    if (error.response) {
-        return res.sendStatus(error.response.status);
-    }
-    return res.sendStatus(500);
+    console.log(error)
+	if (error.message) {
+		return res.status(400).send(error.message);
+	}
+	return res.sendStatus(500);
 }
